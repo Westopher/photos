d0f47175c8a5d5f1ce173ed0be83719fcd965217
West Kraemer <kraemerwest@gmail.com>
2017-11-29 12:36:38 -0500

    use saved photos

--- Photos/AddItemViewController.swift
+++ Photos/AddItemViewController.swift
@@ -8,21 +8,36 @@
 
 import UIKit
 
-class AddItemViewController: UIViewController {
+class AddItemViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     
     @IBOutlet var itemImageView: UIImageView!
     @IBOutlet var titleTextField: UITextField!
     
+    var imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        imagePicker.delegate = self
 
     }
 
     @IBAction func photosTapped(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
     }
     
     @IBAction func cameraTapped(_ sender: Any) {
+       // imagePicker.sourceType = .camera
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let chosenImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            itemImageView.image = chosenImage
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
     }
     
     @IBAction func addTapped(_ sender: Any) {
