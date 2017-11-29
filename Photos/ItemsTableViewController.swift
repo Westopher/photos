f06f1e8eea57adfa8bf74b9f2292a5c28a56bfaa
West Kraemer <kraemerwest@gmail.com>
2017-11-29 13:33:23 -0500

    delete table view item with left swipe

--- Photos/ItemsTableViewController.swift
+++ Photos/ItemsTableViewController.swift
@@ -9,12 +9,12 @@
 import UIKit
 
 class ItemsTableViewController: UITableViewController {
-
+    
     var items : [Item] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -22,7 +22,7 @@ class ItemsTableViewController: UITableViewController {
     }
     
     func getItems() {
-         if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+        if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
             
             if let coreDataStuff = try? context.fetch(Item.fetchRequest()) as? [Item] {
                 if let coreDataItems = coreDataStuff {
@@ -34,23 +34,28 @@ class ItemsTableViewController: UITableViewController {
         }
     }
     
-
-
+    
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return items.count
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
+        
         let item = items[indexPath.row]
         
         cell.textLabel?.text = item.title
-
+        
+        //puts a small image in the table view
+        if let imageData = item.image {
+            cell.imageView?.image = UIImage(data: imageData)
+        }
+        
         return cell
     }
-
+    
     
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
@@ -58,15 +63,16 @@ class ItemsTableViewController: UITableViewController {
         return true
     }
     
-
+    
     
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+                let item = items[indexPath.row]
+                context.delete(item)
+                tableView.reloadData()
+            }    
+        }
     }
 }
